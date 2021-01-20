7af1256f3bcac09186a34834b19986696c1fc753
Sesko <sergioibanezmoreno@gmail.com>
2021-01-20 08:19:53 +0000

    Added password digest column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_073632) do
+ActiveRecord::Schema.define(version: 2021_01_20_081600) do
 
   create_table "cases", charset: "latin1", force: :cascade do |t|
     t.text "description"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_073632) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "cases", "cheatsheets"
