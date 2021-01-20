7b2d6e291671aa5028b4172f164ed2485a2ce7dc
Sesko <sergioibanezmoreno@gmail.com>
2021-01-20 08:23:12 +0000

    Added authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
     resources :cheatsheets, only: :index
   end
 
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+
 end
