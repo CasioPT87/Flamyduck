f053a06462d0e329b73108bba4f5a971aa07ed62
Sesko <sergioibanezmoreno@gmail.com>
2021-02-06 18:32:08 +0000

    Reverted Case model and Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :groups, as: :creator
   has_and_belongs_to_many :groups
 
+  has_many :cheatsheets
+
   validates :name, presence: true
   validates_length_of :name, minimum: 3, maximum: 20, allow_blank: false
   
--- db/migrate/20210113085141_create_cases.rb
+++
@@ -1,9 +0,0 @@
-class CreateCases < ActiveRecord::Migration[6.1]
-  def change
-    create_table :cases do |t|
-      t.text :description
-
-      t.timestamps
-    end
-  end
-end
