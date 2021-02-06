2f35a191cb168e0dbac998fb5fd2272cbf33e75a
Sesko <sergioibanezmoreno@gmail.com>
2021-02-06 18:39:15 +0000

    Change migrations to use Scenation instead of Case

--- db/migrate/20210115073332_add_case_to_cheatsheet.rb
+++
@@ -1,5 +0,0 @@
-class AddCaseToCheatsheet < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :cases, :cheatsheet, null: false, foreign_key: true
-  end
-end
---
+++ db/migrate/20210115073332_add_scenario_to_cheatsheet.rb
@@ -0,0 +1,5 @@
+class AddScenarioToCheatsheet < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :scenarios, :cheatsheet, null: false, foreign_key: true
+  end
+end
