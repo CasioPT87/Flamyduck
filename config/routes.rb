cd4146bbb8a4a19601a92628f1cdd5aa142eb2f8
Sesko <sergioibanezmoreno@gmail.com>
2021-02-08 15:22:21 +0000

    Removed controller and route for Scenario

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :cheatsheets do
-    resources :scenarios
-  end
-
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
