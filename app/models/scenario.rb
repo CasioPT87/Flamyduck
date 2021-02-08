0d41c8b8f212d5d0ff3779e2dd9ead82eace4da5
Sesko <sergioibanezmoreno@gmail.com>
2021-02-08 10:36:40 +0000

    Fixing Scenario validation -thanks to the testsgit add

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -3,6 +3,6 @@ class Scenario < ApplicationRecord
   
   belongs_to :cheatsheet
 
-  validates :name, presence: true
-  validates_length_of :name, minimum: 1, maximum: 50, allow_blank: false
+  validates :content, presence: true
+  validates_length_of :content, minimum: 1, maximum: 50, allow_blank: false
 end
