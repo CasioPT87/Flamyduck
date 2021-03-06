e1c0a4d93dcc31ce02725be5113068c7ec0d1d57
Sesko <sergioibanezmoreno@gmail.com>
2021-03-06 09:22:38 +0000

    Added correct validations for sign up user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,14 @@ class UsersController < ApplicationController
   def create
    @user = User.new(params.require(:user).permit(:name, :password))
    if @user.save
+    debugger
     session[:user_id] = @user.id
     flash[:notice] = 'User created successfuly'
     redirect_to '/'
    else
-    flash[:alert] = 'User could not be created'
+    debugger
+    flash[:alert] = @user.errors.full_messages.join(', ')
     redirect_to new_user_path
    end
-   
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,23 @@ class User < ApplicationRecord
 
   has_many :cheatsheets
 
-  validates :name, presence: true
-  validates_length_of :name, minimum: 3, maximum: 20, allow_blank: false
+  validates :name, presence: true, format: { with: /\A[\w-]+\z/, message: "User name can only contain letters, digits, dashes and underscores" }
+  validates_length_of :name, :maximum => 15, :minimum => 3, allow_blank: false, allow_nil: false,
+    message: "User name must be within 3 and 15 characters"
+
+  validates :password, presence: true, allow_blank: false
+  validate :password_requirements_are_met
+
+  def password_requirements_are_met
+    rules = {
+      " must contain at least one lowercase letter"  => /[a-z]+/,
+      " must contain at least one uppercase letter"  => /[A-Z]+/,
+      " must contain at least one digit"             => /\d+/
+    }
+  
+    rules.each do |message, regex|
+      errors.add( :password, message ) unless password.match( regex )
+    end
+  end
   
 end
