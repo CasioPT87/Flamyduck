d89ea7e3bb3a3a6e5f1f684a630d29c5b2ceaa24
Sesko <sergioibanezmoreno@gmail.com>
2021-03-06 11:16:57 +0000

    Added migration to add email to user and working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,10 +18,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    debugger
     @user = current_user
   end
 
   def update 
+    @user = current_user
+    if @user.update(params.require(:user).permit(:name, :password, :email))
+      flash[:notice] = 'User updated successfuly'
+      redirect_to edit_user_path(@user)
+    else
+      flash[:alert] = @user.errors.full_messages.join(', ')
+      redirect_to edit_user_path(@user)
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_093359) do
+ActiveRecord::Schema.define(version: 2021_03_06_110140) do
 
   create_table "action_text_rich_texts", charset: "latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_093359) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "email"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
