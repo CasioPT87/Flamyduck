dae24237aa1191b97fc60420dc5b00af6451b1fe
Sergio <sergioibanezmoreno@gmail.com>
2021-03-07 08:53:09 +0000

    added some semi-colons

--- app/javascript/controllers/drag_item_controller.js
+++ app/javascript/controllers/drag_item_controller.js
@@ -8,36 +8,36 @@ export default class extends Controller {
   // }
 
   dragstart = (event) => {
-    event.dataTransfer.setData("application/drag-key", event.target.getAttribute("data-sort-id"))
-    event.dataTransfer.effectAllowed = "move"
+    event.dataTransfer.setData("application/drag-key", event.target.getAttribute("data-sort-id"));
+    event.dataTransfer.effectAllowed = "move";
   }
 
   dragover(event) {
     const dragElement = event.target;
-    this.dropElement = dragElement.closest(".droppable")
+    this.dropElement = dragElement.closest(".droppable");
     if (this.dropElement) {
-      event.preventDefault()
-      return true
+      event.preventDefault();
+      return true;
     } else {
       return false;
     }
   }
 
   dragenter(event) {
-    event.preventDefault()
+    event.preventDefault();
   }
 
   drop(event) {
-    var data = event.dataTransfer.getData("application/drag-key")
-    const dropTarget = this.dropElement
+    var data = event.dataTransfer.getData("application/drag-key");
+    const dropTarget = this.dropElement;
     const draggedItem = this.element.querySelector(`[data-sort-id='${data}']`);
-    const positionComparison = dropTarget.compareDocumentPosition(draggedItem)
+    const positionComparison = dropTarget.compareDocumentPosition(draggedItem);
     if ( positionComparison & 4) {
       dropTarget.insertAdjacentElement('beforebegin', draggedItem);
     } else if ( positionComparison & 2) {
       dropTarget.insertAdjacentElement('afterend', draggedItem);
     }
-    event.preventDefault()
+    event.preventDefault();
   }
 
   dragend(event) {
